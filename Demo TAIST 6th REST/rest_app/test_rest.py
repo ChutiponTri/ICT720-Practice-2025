4fe376f0b67f8a608d9451e2a0e8494d1b005818
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2025-03-15 11:33:19 +0700

    Add files via upload

---
+++ Demo TAIST 6th REST/rest_app/test_rest.py
@@ -0,0 +1,15 @@
+import requests
+
+# station_id = "esp32"          # Station ID
+# url = f"http://192.168.137.1:5000/api/station/{station_id}"
+
+asset_id = "Ton-M5StickC-0"     # Asset ID is a device name
+url = f"http://192.168.137.1:5000/api/asset/{asset_id}"
+
+resp = requests.get(url=url)
+if resp.status_code == 200:
+    print(resp.json())
+else:
+    print(resp.status_code, resp.text)
+
+
