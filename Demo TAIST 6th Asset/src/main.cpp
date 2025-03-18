d83d1ab7c58fcff99a87f76fa369a156fe370aa1
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2025-03-18 15:29:55 +0700

    Update main.cpp

--- Demo TAIST 6th Asset/src/main.cpp
+++ Demo TAIST 6th Asset/src/main.cpp
@@ -3,7 +3,11 @@
 #include <BLEAdvertising.h>
 #include <M5StickC.h>
 
-void show_qr_code(String name);
+#define name0 "Asset-T6"
+#define name1 "Asset-T7"
+#define name2 "Asset-T8"
+
+void show_qr_code(String name, String url);
 
 void setup(void) {
   // init HW
@@ -11,13 +15,13 @@ void setup(void) {
   M5.Lcd.begin();
   
   // init BLE
-  BLEDevice::init("Ton-M5StickC-0");
+  BLEDevice::init(name0);
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  show_qr_code("Ton-M5StickC-0");
+  show_qr_code("TON-M5", "www.24av.net");
 }
 
 void loop(void) {
@@ -25,27 +29,26 @@ void loop(void) {
   if (M5.BtnA.wasPressed()) {
     // change broadcast name
     BLEDevice::stopAdvertising();
-    esp_ble_gap_set_device_name("Ton-M5StickC-1");
+    esp_ble_gap_set_device_name(name1);
     BLEDevice::startAdvertising(); 
-    show_qr_code("Ton-M5StickC-1");
+    show_qr_code("TON-M6", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   }
   if (M5.BtnB.wasPressed()) {
     // change broadcast name
     BLEDevice::stopAdvertising();
-    esp_ble_gap_set_device_name("Ton-M5StickC-2");
+    esp_ble_gap_set_device_name(name2);
     BLEDevice::startAdvertising(); 
-    show_qr_code("Ton-M5StickC-2");
+    show_qr_code("TON-M7", "https://www.youtube.com/watch?v=9BalEldzE8o");
   }
   delay(100);
 }
 
-void show_qr_code(String name) {
+void show_qr_code(String name, String url) {
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(5, 5);
   M5.Lcd.println(name);
   uint16_t sz = 0;
-  M5.Lcd.qrcode(name, 0, 80, 80);
+  M5.Lcd.qrcode(url, 0, 80, 80);
 }
-
